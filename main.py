fef87f7f7bdf6ad9f3b69ae4cca0d12d2d88ebe1
Tomi Chen <tctree333@users.noreply.github.com>
2019-08-04 12:02:06 -0700

    fix something

--- main.py
+++ main.py
@@ -648,7 +648,7 @@ async def leaderboard(ctx, placings = 3):
 async def test(ctx):
   embed = discord.Embed(title="Leaderboard:", type="rich", colour=discord.Color.blurple())
   embed.add_field(name="Leaderboard", value="<@289965680554672149>")
-  ctx.send(embed=embed)
+  await ctx.send(embed=embed)
 
 ######
 # ERROR CHECKING
