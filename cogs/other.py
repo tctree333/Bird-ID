49f1cf39d08a32e7fa94d1ecf2a1eefcbef42d1e
Tomi Chen <tctree333@users.noreply.github.com>
2019-08-21 20:38:17 -0700

    update invite links

--- cogs/other.py
+++ cogs/other.py
@@ -64,12 +64,13 @@ class Other(commands.Cog):
                         value="This bot was created by EraserBird and person_v1.32 for helping people practice bird identification for Science Olympiad.",
                         inline=False)
         embed.add_field(name="Support",
-                        value="If you are experiencing any issues, have feature requests, or want to get updates on bot status, join our support server at https://discord.gg/fXxYyDJ",
+                        value="If you are experiencing any issues, have feature requests, or want to get updates on bot status, join our support server below.",
                         inline=False)
         embed.add_field(name="Stats",
                         value=f"This bot can see {len(self.bot.users)} users and is in {len(self.bot.guilds)} servers. The WebSocket latency is {str(round(self.bot.latency, 2))}.",
                         inline=False)
         await ctx.send(embed=embed)
+        await ctx.send("https://discord.gg/fXxYyDJ")
 
     # invite command - sends invite link
     @commands.command(help="- get the invite link for this bot")
@@ -86,9 +87,10 @@ class Other(commands.Cog):
                         value="""To invite this bot to your own server, use the following invite links.\n
                         **Bird-ID:** https://discordapp.com/api/oauth2/authorize?client_id=601917808137338900&permissions=51200&scope=bot\n
                         **Orni-Bot:** https://discordapp.com/api/oauth2/authorize?client_id=601755752410906644&permissions=51200&scope=bot\n
-                        For more information on the differences between the two bots, visit our support server at https://discord.gg/fXxYyDJ""",
+                        For more information on the differences between the two bots, visit our support server below.""",
                         inline=False)
         await ctx.send(embed=embed)
+        await ctx.send("https://discord.gg/fXxYyDJ")
 
     # Test command - for testing purposes only
     @commands.command(help="- test command", hidden=True)
