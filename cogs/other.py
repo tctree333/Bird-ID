3a397d3483ffc458adcdf8405c8a23fbefdb3c1f
Tomi Chen <tctree333@users.noreply.github.com>
2019-08-21 20:01:23 -0700

    add meta commands

--- cogs/other.py
+++ cogs/other.py
@@ -49,8 +49,49 @@ class Other(commands.Cog):
         x = randint(0, len(memeList))
         await ctx.send(memeList[x])
 
+    # bot info command - gives info on bot
+    @commands.command(help="- gives info on bot, support server invite", aliases=["bot_info", "support"])
+    @commands.cooldown(1, 5.0, type=commands.BucketType.channel)
+    async def botinfo(self, ctx):
+        print("bot info")
+        
+        await channel_setup(ctx)
+        await user_setup(ctx)
+
+        embed = discord.Embed(type="rich", colour=discord.Color.blurple())
+        embed.set_author(name="Bird ID - An Ornithology Bot")
+        embed.add_field(name="Bot Info",
+                        value="This bot was created by EraserBird and person_v1.32 for helping people practice bird identification for Science Olympiad.",
+                        inline=False)
+        embed.add_field(name="Support",
+                        value="If you are experiencing any issues, have feature requests, or want to get updates on bot status, join our support server at https://discord.gg/fXxYyDJ",
+                        inline=False)
+        embed.add_field(name="Stats",
+                        value=f"This bot can see {len(self.bot.users)} users and is in {len(self.bot.guilds)} servers. The WebSocket latency is {str(round(self.bot.latency, 2))}.",
+                        inline=False)
+        await ctx.send(embed=embed)
+
+    # invite command - sends invite link
+    @commands.command(help="- get the invite link for this bot")
+    @commands.cooldown(1, 5.0, type=commands.BucketType.channel)
+    async def invite(self, ctx):
+        print("invite")
+        
+        await channel_setup(ctx)
+        await user_setup(ctx)
+
+        embed = discord.Embed(type="rich", colour=discord.Color.blurple())
+        embed.set_author(name="Bird ID - An Ornithology Bot")
+        embed.add_field(name="Invite",
+                        value="""To invite this bot to your own server, use the following invite links.\n
+                        **Bird-ID:** https://discordapp.com/api/oauth2/authorize?client_id=601917808137338900&permissions=51200&scope=bot\n
+                        **Orni-Bot:** https://discordapp.com/api/oauth2/authorize?client_id=601755752410906644&permissions=51200&scope=bot\n
+                        For more information on the differences between the two bots, visit our support server at https://discord.gg/fXxYyDJ""",
+                        inline=False)
+        await ctx.send(embed=embed)
+
     # Test command - for testing purposes only
-    @commands.command(help="- test command")
+    @commands.command(help="- test command", hidden=True)
     async def test(self, ctx):
         print("test")
         embed = discord.Embed(type="rich", colour=discord.Color.blurple())
