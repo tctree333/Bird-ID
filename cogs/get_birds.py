95280ae277c3630eba49f9d69342c602accadae8
Tomi Chen <tctree333@users.noreply.github.com>
2019-08-21 20:40:58 -0700

    update prefixes

--- cogs/get_birds.py
+++ cogs/get_birds.py
@@ -34,9 +34,9 @@ class Birds(commands.Cog):
             database.lset(str(ctx.channel.id), 8, str(currentBird))
             database.lset(str(ctx.channel.id), 0, str(currentBird))
             print("currentBird: "+str(currentBird))
-            await send_bird(ctx, currentBird, on_error=error_skip, message="*Here you go!* \n**Use `o>bird` again to get a new picture of the same bird, or `o>skip` to get a new bird. Use `o>check guess` to check your answer. Use `o>hint` for a hint.**", addOn=add_on)
+            await send_bird(ctx, currentBird, on_error=error_skip, message="*Here you go!* \n**Use `b!bird` again to get a new picture of the same bird, or `b!skip` to get a new bird. Use `b!check guess` to check your answer. Use `b!hint` for a hint.**", addOn=add_on)
         else:  # if no, give the same bird
-            await send_bird(ctx, str(database.lindex(str(ctx.channel.id), 0))[2:-1], on_error=error_skip, message="*Here you go!* \n**Use `o>bird` again to get a new picture of the same bird, or `o>skip` to get a new bird. Use `o>check guess` to check your answer.**", addOn=add_on)
+            await send_bird(ctx, str(database.lindex(str(ctx.channel.id), 0))[2:-1], on_error=error_skip, message="*Here you go!* \n**Use `b!bird` again to get a new picture of the same bird, or `b!skip` to get a new bird. Use `b!check guess` to check your answer.**", addOn=add_on)
             database.lset(str(ctx.channel.id), 1, "0")
 
     # goatsucker command - no args
@@ -57,9 +57,9 @@ class Birds(commands.Cog):
             currentBird = goatsuckers[randint(0, 2)]
             database.lset(str(ctx.channel.id), 5, str(currentBird))
             print("currentBird: "+str(currentBird))
-            await send_bird(ctx, currentBird, message="*Here you go!* \n**Use `o>bird` again to get a new picture of the same goatsucker, or `o>skipgoat` to get a new bird. Use `o>checkgoat guess` to check your answer. Use `o>hint` for a hint.**")
+            await send_bird(ctx, currentBird, message="*Here you go!* \n**Use `b!bird` again to get a new picture of the same goatsucker, or `b!skipgoat` to get a new bird. Use `b!checkgoat guess` to check your answer. Use `b!hint` for a hint.**")
         else:  # if no, give the same bird
-            await send_bird(ctx, str(database.lindex(str(ctx.channel.id), 5))[2:-1], message="*Here you go!* \n**Use `o>bird` again to get a new picture of the same bird, or `o>skip` to get a new bird. Use `o>check guess` to check your answer.**")
+            await send_bird(ctx, str(database.lindex(str(ctx.channel.id), 5))[2:-1], message="*Here you go!* \n**Use `b!bird` again to get a new picture of the same bird, or `b!skip` to get a new bird. Use `b!check guess` to check your answer.**")
             database.lset(str(ctx.channel.id), 6, "0")
 
     # picks a random bird call to send
@@ -82,10 +82,10 @@ class Birds(commands.Cog):
             database.lset(str(ctx.channel.id), 9, str(currentSongBird))
             database.lset(str(ctx.channel.id), 2, str(currentSongBird))
             print("currentSongBird: "+str(currentSongBird))
-            await send_birdsong(ctx, currentSongBird, message="*Here you go!* \n**Use `o>song` again to get a new sound of the same bird, or `o>skipsong` to get a new bird. Use `o>checksong guess` to check your answer. Use `o>hintsong` for a hint.**")
+            await send_birdsong(ctx, currentSongBird, message="*Here you go!* \n**Use `b!song` again to get a new sound of the same bird, or `b!skipsong` to get a new bird. Use `b!checksong guess` to check your answer. Use `b!hintsong` for a hint.**")
             database.lset(str(ctx.channel.id), 3, "0")
         else:
-            await send_birdsong(ctx, str(database.lindex(str(ctx.channel.id), 2))[2:-1], message="*Here you go!* \n**Use `o>song` again to get a new sound of the same bird, or `o>skipsong` to get a new bird. Use `o>checksong guess` to check your answer. Use `o>hintsong` for a hint.**")
+            await send_birdsong(ctx, str(database.lindex(str(ctx.channel.id), 2))[2:-1], message="*Here you go!* \n**Use `b!song` again to get a new sound of the same bird, or `b!skipsong` to get a new bird. Use `b!checksong guess` to check your answer. Use `b!hintsong` for a hint.**")
             database.lset(str(ctx.channel.id), 3, "0")
 
 
