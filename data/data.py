16ba6d04aca3ad32ba95562aff052a4e1ce9679c
Tomi Chen <tctree333@users.noreply.github.com>
2019-10-18 18:53:48 -0700

    fix meme list casing

--- data/data.py
+++ data/data.py
@@ -64,27 +64,34 @@ logger = logging.getLogger("bird-id")
 logger.setLevel(logging.DEBUG)
 os.makedirs("logs", exist_ok=True)
 
-file_handler = logging.handlers.TimedRotatingFileHandler("logs/log.txt", backupCount=4, when="midnight")
+file_handler = logging.handlers.TimedRotatingFileHandler(
+    "logs/log.txt", backupCount=4, when="midnight")
 file_handler.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
 
-file_handler.setFormatter(logging.Formatter("{asctime} - {filename:10} -  {levelname:8} - {message}", style="{"))
-stream_handler.setFormatter(logging.Formatter("{filename:10} -  {levelname:8} - {message}", style="{"))
+file_handler.setFormatter(logging.Formatter(
+    "{asctime} - {filename:10} -  {levelname:8} - {message}", style="{"))
+stream_handler.setFormatter(logging.Formatter(
+    "{filename:10} -  {levelname:8} - {message}", style="{"))
 
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
 # log uncaught exceptions
 
+
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
 
-    logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    logger.critical("Uncaught exception", exc_info=(
+        exc_type, exc_value, exc_traceback))
+
 
 sys.excepthook = handle_exception
+
 
 class GenericError(commands.CommandError):
     def __init__(self, message=None, code=0):
@@ -98,18 +105,23 @@ class GenericError(commands.CommandError):
 # 999 - Invalid
 # 100 - Blank
 
+
 # Lists of birds, memes, and other info
 goatsuckers = ["Common Pauraque", "Chuck-will's-widow", "Whip-poor-will"]
-sciGoat = ["Nyctidromus albicollis", "Antrostomus carolinensis", "Antrostomus vociferus"]
+sciGoat = ["Nyctidromus albicollis",
+           "Antrostomus carolinensis", "Antrostomus vociferus"]
+
 
 def _nats_lists():
-    filenames = ("birdList", "sciBirdList", "memeList", "songBirds", "sciSongBirds")
+    filenames = ("birdList", "sciBirdList", "memeList",
+                 "songBirds", "sciSongBirds")
     # Converts txt file of data into lists
     lists = []
     for filename in filenames:
         logger.info(f"Working on {filename}")
         with open(f'data/{filename}.txt', 'r') as f:
-            lists.append([string.capwords(line.strip().replace("-", " ")) for line in f])
+            lists.append([string.capwords(line.strip().replace("-", " ")) if filename is not "memeList"
+                          else line.strip() for line in f])
         logger.info(f"Done with {filename}")
     logger.info("Done with nats list!")
     return lists
@@ -128,8 +140,10 @@ def _nats_lists():
 # sci lists are only for new, state specific birds
 # either lists can be in any order
 
+
 def _state_lists():
-    filenames = ("birdList", "sciBirdList", "aliases", "songBirds", "sciSongBirds")
+    filenames = ("birdList", "sciBirdList", "aliases",
+                 "songBirds", "sciSongBirds")
     states = {}
     state_names = os.listdir("data/state")
     for state in state_names:
@@ -139,13 +153,15 @@ def _state_lists():
             logger.info(f"Working on {filename}")
             with open(f'data/state/{state}/{filename}.txt', 'r') as f:
                 states[state][filename] = [
-                    string.capwords(line.strip().replace("-", " ")) if filename is not "aliases" else line.strip()
+                    string.capwords(line.strip().replace(
+                        "-", " ")) if filename is not "aliases" else line.strip()
                     for line in f if line != "EMPTY"
                 ]
             logger.info(f"Done with {filename}")
         logger.info(f"Done with {state}")
     logger.info("Done with states list!")
     return states
+
 
 def _all_birds():
     lists = (birdList, sciBirdList, songBirds, sciSongBirds)
@@ -163,10 +179,12 @@ def _all_birds():
     logger.info("Done with master lists!")
     return master_lists
 
+
 birdList, sciBirdList, memeList, songBirds, sciSongBirds = _nats_lists()
 states = _state_lists()
 birdListMaster, sciBirdListMaster, songBirdsMaster, sciSongBirdsMaster = _all_birds()
-logger.info(f"National Lengths: {len(birdList)}, {len(sciBirdList)}, {len(songBirds)}, {len(sciSongBirds)}")
+logger.info(
+    f"National Lengths: {len(birdList)}, {len(sciBirdList)}, {len(songBirds)}, {len(sciSongBirds)}")
 logger.info(
     f"Master Lengths: {len(birdListMaster)}, {len(sciBirdListMaster)}, {len(songBirdsMaster)}, {len(sciSongBirdsMaster)}"
 )
