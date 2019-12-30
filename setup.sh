08fcc215788bfe6f4c432fb18b30aa16f1af8f42
Tomi Chen <tctree333@users.noreply.github.com>
2019-12-29 17:37:56 -0800

    add documentation for running the bot locally

---
+++ setup.sh
@@ -0,0 +1,14 @@
+echo Exporting Enviornment Variables
+export token="YOUR BOT TOKEN"
+echo Exported token as $token
+
+export ids="YOUR DISCORD USER ID"
+echo Exported ids as $ids
+
+export DISCORD_CLIENT_SECRET="YOUR CLIENT SECRET"
+echo Exported DISCORD_CLIENT_SECRET as $DISCORD_CLIENT_SECRET
+
+# export SENTRY_DISCORD_DSN="OPTIONAL: YOUR SENTRY DSN"
+# echo Exported SENTRY_DISCORD_DSN as $SENTRY_DISCORD_DSN
+
+echo Done!
