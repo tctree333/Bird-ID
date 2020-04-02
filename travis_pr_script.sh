5f479d0643f7f1c667f5bbf46d45047b8f074481
Tomi Chen <tctree333@users.noreply.github.com>
2020-04-01 22:17:55 -0700

    disable sentry on travis prs

--- travis_pr_script.sh
+++ travis_pr_script.sh
@@ -2,4 +2,5 @@ echo "Starting Local Redis Server"
 bash ./install-redis.sh
 redis-server &
 export SCIOLY_ID_BOT_LOCAL_REDIS="true"
+export SCIOLY_ID_BOT_USE_SENTRY="false"
 echo "Redis Server Started"
