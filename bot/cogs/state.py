a9011e0fc3be7a30e1de4351f33cf9da66d51b1c
Tomi Chen <tctree333@users.noreply.github.com>
2020-05-12 00:07:28 -0700

    update state commands

--- bot/cogs/state.py
+++ bot/cogs/state.py
@@ -37,19 +37,22 @@ class States(commands.Cog):
         await channel_setup(ctx)
         await user_setup(ctx)
 
-        roles = [role.name.lower() for role in ctx.author.roles]
+        raw_roles = ctx.author.roles
+        role_ids = [role.id for role in raw_roles]
+        role_names = [role.name.lower() for role in ctx.author.roles]
         args = args.upper().split(" ")
 
+        added = []
+        removed = []
+        invalid = []
         for arg in args:
             if arg not in states:
                 logger.info("invalid state")
-                await ctx.send(
-                    f"**Sorry, `{arg}` is not a valid state.**\n*Valid States:* `{', '.join(map(str, list(states.keys())))}`"
-                )
+                invalid.append(arg)
 
             # gets similarities
-            elif not set(roles).intersection(set(states[arg]["aliases"])):
-                # need to add roles (does not have role)
+            elif not set(role_names).intersection(set(states[arg]["aliases"])):
+                # need to add role (does not have role)
                 logger.info("add roles")
                 raw_roles = ctx.guild.roles
                 guild_role_names = [role.name.lower() for role in raw_roles]
@@ -72,54 +75,29 @@ class States(commands.Cog):
                     )
 
                 await ctx.author.add_roles(role, reason="Set state role for bird list")
-                await ctx.send(f"**Ok, added the {role.name} role!**")
+                added.append(role.name)
 
             else:
                 # have roles already (there were similarities)
-                logger.info("already has role")
-                await ctx.send(f"**You already have the `{arg}` role!**")
+                logger.info("already has role, removing")
+                index = role_names.index(states[arg]["aliases"][0].lower())
+                role = ctx.guild.get_role(role_ids[index])
+                await ctx.author.remove_roles(role, reason="Remove state role for bird list")
+                removed.append(role.name)
 
-    # removes state role
-    @commands.command(help="- Removes your state", aliases=["rm"])
-    @commands.check(CustomCooldown(5.0, bucket=commands.BucketType.user))
-    @commands.guild_only()
-    async def remove(self, ctx, *, args):
-        logger.info("command: remove")
-
-        await channel_setup(ctx)
-        await user_setup(ctx)
-
-        raw_roles = ctx.author.roles
-        user_role_names = [role.name.lower() for role in raw_roles]
-        user_role_ids = [role.id for role in raw_roles]
-        args = args.upper().split(" ")
-
-        for arg in args:
-            if arg not in states:
-                logger.info("invalid state")
-                await ctx.send(
-                    f"**Sorry, `{arg}` is not a valid state.**\n*Valid States:* `{', '.join(map(str, list(states.keys())))}`"
-                )
-
-            elif states[arg]["aliases"][0].lower() not in user_role_names[1:]:
-                logger.info("doesn't have role")
-                await ctx.send(
-                    f"**You don't have the `{arg}` state role!**\n*Your Roles:* `{', '.join(map(str, user_role_names[1:]))}`"
-                )
-
-            else:
-                logger.info("deleting role")
-                index = user_role_names.index(states[arg]["aliases"][0].lower())
-                role = ctx.guild.get_role(user_role_ids[index])
-                await ctx.author.remove_roles(role, reason="Delete state role for bird list")
-                await ctx.send(f"**Ok, role {role.name} deleted!**")
+        await ctx.send(
+            (f"**Sorry,** `{'`, `'.join(invalid)}` **{'are' if len(invalid) > 1 else 'is'} not a valid state.**\n" +
+            f"*Valid States:* `{'`, `'.join(states.keys())}`\n" if invalid else "") +
+            (f"**Added the** `{'`, `'.join(added)}` **role{'s' if len(added) > 1 else ''}**\n" if added else "") +
+            (f"**Removed the** `{'`, `'.join(removed)}` **role{'s' if len(removed) > 1 else ''}**\n" if removed else "")
+        )
 
     @state.error
     async def set_error(self, ctx, error):
         logger.info("state set error")
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(
-                f"**Please enter your state.**\n*Valid States:* `{', '.join(map(str, list(states.keys())))}`"
+                f"**Please enter your state.**\n*Valid States:* `{'`, `'.join(states.keys())}`"
             )
         elif isinstance(error, commands.CommandOnCooldown):  # send cooldown
             await ctx.send("**Cooldown.** Try again after " + str(round(error.retry_after)) + " s.", delete_after=5.0)
@@ -158,53 +136,6 @@ class States(commands.Cog):
             await ctx.send(
                 "**An uncaught set error has occurred.**\n" +
                 "*Please log this message in #support in the support server below, or try again.*\n" + "**Error:** " +
-                str(error)
-            )
-            await ctx.send("https://discord.gg/fXxYyDJ")
-            raise error
-
-    @remove.error
-    async def remove_error(self, ctx, error):
-        logger.info("remove error")
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"**Please enter a state.**\n*Valid States:* `{', '.join(map(str, list(states.keys())))}`")
-        elif isinstance(error, commands.CommandOnCooldown):  # send cooldown
-            await ctx.send("**Cooldown.** Try again after " + str(round(error.retry_after)) + " s.", delete_after=5.0)
-        elif isinstance(error, commands.NoPrivateMessage):
-            await ctx.send("**This command is unavaliable in DMs!**")
-        elif isinstance(error, commands.BotMissingPermissions):
-            await ctx.send(
-                "**The bot does not have enough permissions to fully function.**\n" +
-                f"**Permissions Missing:** `{', '.join(map(str, error.missing_perms))}`\n" +
-                "*Please try again once the correct permissions are set.*"
-            )
-        elif isinstance(error, GenericError):
-            if error.code == 192:
-                #channel is ignored
-                return
-            elif error.code == 842:
-                await ctx.send("**Sorry, you cannot use this command.**")
-            elif error.code == 666:
-                logger.info("GenericError 666")
-            elif error.code == 201:
-                logger.info("HTTP Error")
-                capture_exception(error)
-                await ctx.send("**An unexpected HTTP Error has occurred.**\n *Please try again.*")
-            else:
-                logger.info("uncaught generic error")
-                capture_exception(error)
-                await ctx.send(
-                    "**An uncaught generic error has occurred.**\n" +
-                    "*Please log this message in #support in the support server below, or try again.*\n" +
-                    "**Error:** " + str(error)
-                )
-                await ctx.send("https://discord.gg/fXxYyDJ")
-                raise error
-        else:
-            capture_exception(error)
-            await ctx.send(
-                "**An uncaught remove error has occurred.**\n" +
-                "*Please log this message in  #support in the support server below, or try again.*\n" + "**Error: ** " +
                 str(error)
             )
             await ctx.send("https://discord.gg/fXxYyDJ")
