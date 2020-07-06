3eeffae5826094271a8efc62c7a450a4e6d84a01
Tomi Chen <tctree333@users.noreply.github.com>
2020-07-06 16:56:39 -0700

    update filter parse

--- bot/filters.py
+++ bot/filters.py
@@ -227,8 +227,7 @@ class Filter:
         me._clear()  # reset existing filters to empty
         lookup = me.aliases(lookup=True)
         if not use_numbers:
-            lookup = {k if not isinstance(k, int) else None :i for k, i in lookup.items()}
-            lookup.pop(None)
+            lookup = {k:i for k, i in lookup.items() if not k.isdecimal()}
         args = args.lower().strip()
         if "," in args:
             inputs = map(lambda x: x.strip(), args.split(","))
