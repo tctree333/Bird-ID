8f33a5194309b7372e906cb2d5b09d4c0efaf4e8
Tomi Chen <tctree333@users.noreply.github.com>
2020-10-19 18:21:43 -0700

    voice commands

--- bot/cogs/voice.py
+++ bot/cogs/voice.py
@@ -14,11 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import asyncio
-
-import discord
 from discord.ext import commands
 
+import bot.voice as voice_functions
 from bot.data import logger
 from bot.functions import CustomCooldown
 
@@ -27,117 +25,33 @@ class Voice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_voice_client(self, ctx, connect=False):
-        voice = ctx.author.voice
-        if voice is None or voice.channel is None:
-            await ctx.send("**Please join a voice channel to connect the bot!**")
-            return None
-        client = next(
-            filter(lambda x: x.guild == voice.channel.guild, self.bot.voice_clients), None
-        )
-        if client is None:
-            if connect:
-                try:
-                    client = await voice.channel.connect()
-                    await ctx.send(f"Connected to {voice.channel.mention}")
-                    return client
-                except asyncio.TimeoutError:
-                    await ctx.send(
-                        "**Could not connect to voice channel in time.**\n*Please try again.*"
-                    )
-                except discord.ClientException:
-                    await ctx.send("**I'm already connected to another voice channel!**")
-                return None
-            await ctx.send("**The bot isn't in a voice channel!**")
-            return None
-        if client.channel != voice.channel:
-            await ctx.send("**You need to be in the same voice channel as the bot!**")
-            return None
-        return client
-
-    # @commands.command(help="- Connects to a voice channel")
-    # @commands.check(CustomCooldown(3.0, bucket=commands.BucketType.channel))
-    # @commands.guild_only()
-    # async def connect(self, ctx):
-    #     logger.info("command: connect")
-
-    #     voice = ctx.author.voice
-    #     if voice is None or voice.channel is None:
-    #         await ctx.send("Please join a voice channel to connect the bot!")
-    #         return
-    #     try:
-    #         await voice.channel.connect()
-    #         await ctx.send(f"Connected to {voice.channel.mention}")
-    #     except asyncio.TimeoutError:
-    #         await ctx.send(
-    #             "**Could not connect to voice channel in time.**\n*Please try again.*"
-    #         )
-    #         return
-    #     except discord.ClientException:
-    #         await ctx.send("**I'm already connected to another voice channel!**")
-    #         return
-
     @commands.command(help="- Play a sound")
     @commands.check(CustomCooldown(3.0, bucket=commands.BucketType.channel))
     @commands.guild_only()
     async def play(self, ctx):
         logger.info("command: play")
-
-        client = await self.get_voice_client(ctx, connect=True)
-        if client is None:
-            return
-        if client.is_paused():
-            client.resume()
-            await ctx.send("Resumed playing.")
-            return
-        await ctx.send("Playing...")
-        source = await discord.FFmpegOpusAudio.from_probe("rick.mp3")
-        client.play(source)
+        await voice_functions.play(ctx, "rick.mp3")
 
     @commands.command(help="- Pause playing")
     @commands.check(CustomCooldown(3.0, bucket=commands.BucketType.channel))
     @commands.guild_only()
     async def pause(self, ctx):
         logger.info("command: pause")
-
-        client = await self.get_voice_client(ctx)
-        if client is None:
-            return
-        if client.is_playing():
-            client.pause()
-            await ctx.send("Paused.")
-        elif client.is_paused():
-            await ctx.send("Already paused.")
-        else:
-            await ctx.send("There's nothing playing!")
+        await voice_functions.pause(ctx)
 
     @commands.command(help="- Stop playing")
     @commands.check(CustomCooldown(3.0, bucket=commands.BucketType.channel))
     @commands.guild_only()
     async def stop(self, ctx):
         logger.info("command: stop")
-
-        client = await self.get_voice_client(ctx)
-        if client is None:
-            return
-        if client.is_playing() or client.is_paused():
-            client.stop()
-            await ctx.send("Stopped playing.")
-        else:
-            await ctx.send("There's nothing playing!")
+        await voice_functions.stop(ctx)
 
     @commands.command(help="- Disconnect from voice", aliases=["dc"])
     @commands.check(CustomCooldown(3.0, bucket=commands.BucketType.channel))
     @commands.guild_only()
     async def disconnect(self, ctx):
         logger.info("command: disconnect")
-
-        client = await self.get_voice_client(ctx)
-        if client is None:
-            return
-        client.stop()
-        await client.disconnect()
-        await ctx.send("Bye!")
+        await voice_functions.disconnect(ctx)
 
 
 def setup(bot):
