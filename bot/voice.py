6bf70594fe7b9f49eaeae3d823859da14804ace3
Tomi Chen <tctree333@users.noreply.github.com>
2020-10-19 18:21:30 -0700

    voice functions

---
+++ bot/voice.py
@@ -0,0 +1,125 @@
+# voice.py | functions for voice
+# Copyright (C) 2019-2020  EraserBird, person_v1.32, hmmm
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import asyncio
+from typing import Optional
+
+import discord
+
+from bot.data import logger
+
+
+async def _send(ctx, silent, message: str):
+    if not silent:
+        await ctx.send(message)
+
+
+async def get_voice_client(
+    ctx, connect: bool = False, silent: bool = False
+) -> Optional[discord.VoiceClient]:
+    voice = ctx.author.voice
+    if voice is None or voice.channel is None:
+        await _send(ctx, silent, "**Please join a voice channel to connect the bot!**")
+        return None
+    client: discord.VoiceClient = next(
+        filter(lambda x: x.guild == voice.channel.guild, ctx.bot.voice_clients), None
+    )
+    if client is None:
+        if connect:
+            try:
+                client = await voice.channel.connect()
+                await _send(ctx, silent, f"Connected to {voice.channel.mention}")
+                return client
+            except asyncio.TimeoutError:
+                await _send(
+                    ctx,
+                    silent,
+                    "**Could not connect to voice channel in time.**\n*Please try again.*",
+                )
+            except discord.ClientException:
+                await _send(
+                    ctx, silent, "**I'm already connected to another voice channel!**"
+                )
+        else:
+            await _send(ctx, silent, "**The bot isn't in a voice channel!**")
+        return None
+    if client.channel != voice.channel:
+        await _send(
+            ctx, silent, "**You need to be in the same voice channel as the bot!**"
+        )
+        return None
+    return client
+
+
+async def play(ctx, file: str, silent: bool = False):
+    logger.info("voice: playing")
+
+    client: discord.VoiceClient = await get_voice_client(ctx, connect=True)
+    if client is None:
+        return False
+    if client.is_paused():
+        client.resume()
+        await _send(ctx, silent, "Resumed playing.")
+        return
+    source = await discord.FFmpegOpusAudio.from_probe(file)
+    if client.is_playing():
+        client.stop()
+    client.play(source)
+    await _send(ctx, silent, "Playing...")
+    return True
+
+
+async def pause(ctx, silent: bool = False):
+    logger.info("voice: pausing")
+
+    client: discord.VoiceClient = await get_voice_client(ctx)
+    if client is None:
+        return False
+    if client.is_playing():
+        client.pause()
+        await _send(ctx, silent, "Paused.")
+    elif client.is_paused():
+        await _send(ctx, silent, "Already paused.")
+    else:
+        await _send(ctx, silent, "There's nothing playing!")
+    return True
+
+
+async def stop(ctx, silent: bool = False):
+    logger.info("voice: stopping")
+
+    client: discord.VoiceClient = await get_voice_client(ctx)
+    if client is None:
+        return False
+    if client.is_playing() or client.is_paused():
+        client.stop()
+        await _send(ctx, silent, "Stopped playing.")
+    else:
+        await _send(ctx, silent, "There's nothing playing!")
+    return True
+
+
+async def disconnect(ctx, silent: bool = False):
+    logger.info("voice: disconnecting")
+
+    client: discord.VoiceClient = await get_voice_client(ctx)
+    if client is None:
+        return False
+    client.stop()
+    await client.disconnect()
+    client.cleanup()
+    await _send(ctx, silent, "Bye!")
+    return True
