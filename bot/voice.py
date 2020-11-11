bbb0e7f2740984e1c335740ebb3f2d7dd61b6fe8
Tomi Chen <tctree333@users.noreply.github.com>
2020-11-10 17:02:00 -0800

    try different audio approach

--- bot/voice.py
+++ bot/voice.py
@@ -99,15 +99,24 @@ async def play(ctx, filename: str, silent: bool = False):
     if client.is_paused():
         client.resume()
         t = client.source.remaining
-        await _send(ctx, silent, f"**Resumed playing.** `{t//3600:0>2}:{(t//60)%60:0>2}:{t%60:0>2} remaining`")
+        await _send(
+            ctx,
+            silent,
+            f"**Resumed playing.** `{t//3600:0>2}:{(t//60)%60:0>2}:{t%60:0>2} remaining`",
+        )
         return
     # source = await discord.FFmpegOpusAudio.from_probe(filename)
-    source = CustomAudio(filename)
+    # source = CustomAudio(filename)
+    source = await CustomFFmpegAudio.from_probe(filename)
     if client.is_playing():
         client.stop()
     client.play(source)
     t = source.length
-    await _send(ctx, silent, f"**Playing...** `{t//3600:0>2}:{(t//60)%60:0>2}:{t%60:0>2} remaining`")
+    await _send(
+        ctx,
+        silent,
+        f"**Playing...** `{t//3600:0>2}:{(t//60)%60:0>2}:{t%60:0>2} remaining`",
+    )
     return True
 
 
@@ -164,13 +173,14 @@ async def rel_seek(ctx, seconds: Optional[int], silent: bool = False):
         client.source.jump(seconds)
         if not (client.is_playing() or client.is_paused):
             client.play(client.source)
+        t = client.source.remaining
         await _send(
             ctx,
             silent,
             (
-                f"**Skipped {'forward' if seconds > 0 else 'back'} {abs(seconds)} seconds!**"
+                f"**Skipped {'forward' if seconds > 0 else 'back'} {abs(seconds)} seconds!** `{t//3600:0>2}:{(t//60)%60:0>2}:{t%60:0>2} remaining`"
                 if seconds
-                else "**Restarted from beginning.**"
+                else f"**Restarted from beginning.** `{t//3600:0>2}:{(t//60)%60:0>2}:{t%60:0>2} remaining`"
             ),
         )
     else:
@@ -202,38 +212,72 @@ async def cleanup(bot):
     logger.info("done cleaning!")
 
 
+class CustomFFmpegAudio(discord.FFmpegOpusAudio):
+    def __init__(self, filename, bitrate, codec):
+        super().__init__(filename, bitrate=bitrate, codec=codec)
+        self._data_list_ = list(self._packet_iter)
+        self._data_list_.append(b"")
+        self._cursor = 0
+
+    @property
+    def length(self):
+        return round(len(self._data_list_) * 0.02)
+
+    @property
+    def remaining(self):
+        return round((len(self._data_list_) - self._cursor) * 0.02)
+
+    def jump(self, seconds: Optional[int]):
+        if seconds is None:
+            self._cursor = 0
+            return self
+
+        seconds *= 50  # each cursor tick is 20ms, convert seconds to 20ms chunks
+        if self._cursor + seconds < 0:
+            self._cursor = 0
+        elif self._cursor + seconds > len(self._data_list_):
+            self._cursor = len(self._data_list_)
+        else:
+            self._cursor += seconds
+        return self
+
+    def read(self):
+        self._cursor += 1
+        return self._data_list_[self._cursor - 1]
+
+
 class CustomAudio(discord.AudioSource):
     def __init__(self, filename):
         self.filename = filename
-        self.cursor = 0
+        self._cursor = 0
         self.segment = pydub.AudioSegment.from_file(
             filename, format=filename.split(".")[-1]
         ).set_frame_rate(48000)
 
     @property
     def length(self):
-        return round(len(self.segment)/1000)
+        return round(len(self.segment) / 1000)
 
     @property
     def remaining(self):
-        return round((len(self.segment) - self.cursor)/1000)
+        return round((len(self.segment) - self._cursor) / 1000)
 
     def read(self):
-        self.cursor += 20
-        return self.segment[self.cursor - 20 : self.cursor].raw_data
+        self._cursor += 20
+        return self.segment[self.cursor - 20 : self._cursor].raw_data
 
     def jump(self, seconds: Optional[int]):
         if seconds is None:
-            self.cursor = 0
+            self._cursor = 0
             return self
 
-        seconds *= 1000
-        if self.cursor + seconds < 0:
-            self.cursor = 0
-        elif self.cursor + seconds > len(self.segment):
-            self.cursor = len(self.segment)
+        seconds *= 1000  # convert to milliseconds
+        if self._cursor + seconds < 0:
+            self._cursor = 0
+        elif self._cursor + seconds > len(self.segment):
+            self._cursor = len(self.segment)
         else:
-            self.cursor += seconds
+            self._cursor += seconds
         return self
 
     def is_opus(self):
