003a4cbc29594edcc361ae8cfbbd3e2251fc9b92
Tomi Chen <tctree333@users.noreply.github.com>
2021-01-16 20:16:25 -0800

    migrate tools to fastapi

--- web/tools.py
+++ web/tools.py
@@ -14,40 +14,35 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import asyncio
 import urllib.parse
 from io import BytesIO
 
 import aiohttp
-import flask
+from fastapi import APIRouter, HTTPException
 
 from bot.core import _black_and_white
 from web.data import logger
+from web.config import NoCacheFileResponse
 
-bp = flask.Blueprint("tools", __name__, url_prefix="/tools")
+router = APIRouter(prefix="/tools", tags=["tools"])
 
 valid_endpoints = "test.cdn.download.ams.birds.cornell.edu"
 valid_content_types = ("image/png", "image/jpeg")
 
 
-@bp.route("/bw", methods=["GET"])
-def convert_bw():
+@router.get("/bw")
+async def convert_bw(url: str):
     logger.info("endpoint: convert bw")
-    url = flask.request.args.get("url", default=None, type=str)
     logger.info(f"args: url: {url}")
-
-    if not url:
-        logger.info("no url")
-        flask.abort(415, "url not specified")
 
     parsed_url = urllib.parse.urlparse(url)
     logger.info(f"parsed url: {parsed_url}")
     if parsed_url.netloc not in valid_endpoints:
         logger.info("invalid url")
-        flask.abort(415, "invalid url")
+        raise HTTPException(status_code=415, detail="invalid url")
 
-    image, content_type = asyncio.run(_bw_helper(url))
-    return flask.send_file(image, mimetype=content_type)
+    image, content_type = await _bw_helper(url)
+    return NoCacheFileResponse(image, media_type=content_type)
 
 
 async def _bw_helper(url):
@@ -55,10 +50,10 @@ async def _bw_helper(url):
         async with session.get(url) as response:
             if response.status != 200:
                 logger.info("invalid response")
-                flask.abort(response.status, "error fetching url")
+                raise HTTPException(status_code=response.status, detail="error fetching url")
             if response.content_type not in valid_content_types:
                 logger.info("invalid content type")
-                flask.abort(415, "invalid content type")
+                raise HTTPException(status_code=415, detail="invalid content type")
             return (
                 _black_and_white(BytesIO(await response.read())),
                 response.content_type,
